02310b71d9da5546499ae025b355d55fe4d18477
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2024-10-04 19:42:32 +0100

    add deployment files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://resourceforge.net',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'ResourceForge',
